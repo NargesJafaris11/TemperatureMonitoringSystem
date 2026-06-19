b635386cd417998b4140b753a68a37117f9e17d9
nargesjafari <nargesjafarie@gmail.com>
2026-06-19 17:09:44 +0200

    Added xUnit test project and Singleton tests

---
+++ TemperatureMonitoring.Tests/SingletonTests.cs
@@ -0,0 +1,17 @@
+﻿using TemperatureMonitoring.Api.Patterns.Singleton;
+
+namespace TemperatureMonitoring.Tests;
+
+public class SingletonTests
+{
+    [Fact]
+    public void SensorManager_Should_Return_Same_Instance()
+    {
+        // Arrange
+        var instance1 = SensorManager.Instance;
+        var instance2 = SensorManager.Instance;
+
+        // Assert
+        Assert.Same(instance1, instance2);
+    }
+}
