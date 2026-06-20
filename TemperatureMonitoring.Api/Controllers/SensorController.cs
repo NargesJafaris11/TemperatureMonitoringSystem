2f47e9957e6ae84d8b401e49366d49a15fd50676
nargesjafari <nargesjafarie@gmail.com>
2026-06-20 13:31:08 +0200

    Removed FacadeTests and corrected the API response for frontend compatibility

--- TemperatureMonitoring.Api/Controllers/SensorController.cs
+++ TemperatureMonitoring.Api/Controllers/SensorController.cs
@@ -11,9 +11,42 @@ public class SensorController : ControllerBase
     public IActionResult Get()
     {
         var facade = new SensorFacade();
-
         var sensors = facade.LoadSensors();
 
-        return Ok(sensors);
+        var response = new
+        {
+            items = sensors.Select(sensor => new
+            {
+                id = sensor.Id,
+                serial_number = sensor.SerialNumber,
+                name = sensor.Name,
+                last_measurements = sensor.LastMeasurements.Select(measurement => new
+                {
+                    type = measurement.Type,
+                    value = measurement.Value,
+                    unit = measurement.Unit,
+                    timestamp = measurement.Timestamp
+                }),
+                aggregations = new
+                {
+                    temperature = new
+                    {
+                        max_today = sensor.Aggregations.Temperature.MaxToday,
+                        min_today = sensor.Aggregations.Temperature.MinToday,
+                        unit = sensor.Aggregations.Temperature.Unit
+                    },
+                    humidity = new
+                    {
+                        max_today = sensor.Aggregations.Humidity.MaxToday,
+                        min_today = sensor.Aggregations.Humidity.MinToday,
+                        unit = sensor.Aggregations.Humidity.Unit
+                    }
+                },
+                last_measurement_timestamp = sensor.LastMeasurementTimestamp,
+                state = sensor.State
+            })
+        };
+
+        return Ok(response);
     }
 }
